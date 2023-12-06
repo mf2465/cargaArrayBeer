03424fab950ac23bc123147d88c11ba13f75e9b7
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2023-12-06 08:54:55 -0300

    actualizo Codigo de barras id

--- promo.php
+++ promo.php
@@ -4,7 +4,7 @@ function promo_productos(){
 
     return [
         [
-            'id'=>'1',
+            'id'=>'7792798999866',
             'nombre'=> 'Andes Rubia',
             'descripción'=> 'Lata 473 cc',
             'precio'=> 540.77,
@@ -12,21 +12,21 @@ function promo_productos(){
 
         ],
         [
-            'id'=>'2',
+            'id'=>'7798091030299',
             'nombre'=> 'Antares Honey',
             'precio'=> 924.72,
             'descripción'=> 'Lata 473 cc',
             'imagen'=> './beer/antares.jpg'
         ],
         [
-            'id'=>'3',
+            'id'=>'7793147118822',
             'nombre'=> 'Budweiser Pilsener',
             'precio'=> 727.07,
             'descripción'=> 'Lata 473 cc',
             'imagen'=> './beer/budweiser.jpg'
         ],
         [
-            'id'=>'4',
+            'id'=>'7793147571689',
             'nombre'=> 'Imperial Golden',
             'precio'=> 579.46,
             'descripción'=> 'Lata 473 cc',
@@ -34,7 +34,7 @@ function promo_productos(){
 
         ],
         [
-            'id'=>'5',
+            'id'=>'7792798001965',
             'nombre'=> 'Patagonia Amber Lager',
             'precio'=> 1316.90,
             'descripción'=> 'Lata 473 cc',
